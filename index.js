906a2ef4f1857956b6584052abae2b91990e44df
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-04-22 17:49:47 +0200

    mostly documenting

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// USAGE: node index.js https://url.example.com [-p N] [-v]
+// Scrapes website found at given URL, optionnally using N parallel
+// headless chrome instances.
+// The -v flag increases the verbosity. It is not very useful at the moment.
+
 import parseArgv from 'minimist';
 import chromeLauncher from 'chrome-launcher';
 import CRI from 'chrome-remote-interface';
@@ -133,10 +138,15 @@ const createScraperProcess = async ({
   Page.loadEventFired(async () => {
     const doc = await DOM.getDocument();
 
+    // Get the page's title.
+    // This is more convenient to do by executing JS in the host chrome
+    // than with the DOM api.
     const extractTitleJS = 'document.querySelector("title").textContent';
     const titleResult = await Runtime.evaluate({ expression: extractTitleJS });
     console.log(`Reached page: ${titleResult.result.value}`);
 
+    // Check if there is a <link rel="canonical" href="...">
+    // in the <head> of the page.
     const { nodeId: canonicalNodeId } = await DOM.querySelector({
       nodeId: doc.root.nodeId,
       selector: 'head link[rel=canonical]',
@@ -144,16 +154,28 @@ const createScraperProcess = async ({
 
     let ignoreNonCanonicalPage = false;
 
+    // If the page has a canonical URL and we haven't scraped this
+    // canonical URL previously, we add it to the scraping queue.
     if (canonicalNodeId !== 0) {
       const { attributes } = await DOM.getAttributes({ nodeId: canonicalNodeId });
       const attrMap = keyValueArrayToMap(attributes);
       const canonical = normalizeURL(attrMap.get('href'));
       if (seenSet.has(canonical)) {
+        // We have already scraped the canonical URL of the
+        // page we're currently on, so we won't follow any links
+        // on this page.
         ignoreNonCanonicalPage = true;
+      } else {
+        // We've never scraped the canonical page, so we enqueue it.
+        addURLAndScrape(canonical);
       }
-      addURLAndScrape(canonical);
     }
 
+    // Explore the links found on this page,
+    // but only if it has no canonical URL or if its canonical URL
+    // has not yet been scraped.
+    // This may leed to scraping a few too many pages but this feels
+    // safer to me in terms of exhaustivity.
     if (!ignoreNonCanonicalPage) {
       const { nodeIds } = await DOM.querySelectorAll({ nodeId: doc.root.nodeId, selector: 'a' });
 
@@ -161,11 +183,16 @@ const createScraperProcess = async ({
         nodeIds.map((nodeId) => DOM.getAttributes({ nodeId })),
       );
 
+      // getAttributes returns an object of the form:
+      // { attributes: [attr1, value1, attr2, value2, ...]}
+      // We convert all these arrays to javacript Maps for convenience.
       const linkAttributes = linkAttributesArrays.map(
         ({ attributes }) => keyValueArrayToMap(attributes),
       );
 
       for (const attrs of linkAttributes) {
+        // The URL to enqueue is either that of the 'href' attribute
+        // or that of the 'canonical' attribute if there is one.
         const whichURL = attrs.get('canonical') || attrs.get('href');
         const href = normalizeURL(whichURL);
         addURLAndScrape(href, attrs);
@@ -174,7 +201,7 @@ const createScraperProcess = async ({
 
     if (queueSet.size > 0) {
       // If there is work to do,
-      // week doing it.
+      // keep doing it.
       scrape({
         queueSet,
         seenSet,
@@ -190,7 +217,9 @@ const createScraperProcess = async ({
       // the number of running instances and checking
       // if we are done scraping.
       onKill();
+      // Closes the connection to our chrome instance.
       protocol.close();
+      // Terminates the chrome instance.
       chrome.kill();
     }
   });
@@ -204,19 +233,26 @@ const createScraperProcess = async ({
     chromePort: chrome.port,
   });
 
-  // Well only "startScraping" is really useful,
-  // but I thought the calling function might be interested
-  // in accessing the chrome process or the protocol instance.
-  return { chrome, protocol, startScraping };
+  // Returns the method that starts scraping with
+  // the brand new chrome instance.
+  // Returning it as an array because I have a feeling
+  // I might need to expose more variables later.
+  return { startScraping };
 };
 
 // The core of the program.
 // It uses async/await so it needs to be encapsulated in a function.
 const main = async () => {
+  // Set of URLs that remain to be explored.
   const URLQueue = new Set();
+  // Set of URLs that have already been explored.
   const URLSeen = new Set();
+  // Set of URLs that have been found but ignored for some reason.
   const URLIgnored = new Set();
 
+  // Max number of running parallel chrome instances.
+  // Provide it to the cli with the "p" option,
+  // e.g. node index.js https://mysite.example.com -p4
   const nMaxInstances = options.p || 1;
 
   console.log(`Starting scraping of ${startURL} with ${nMaxInstances} parallel chrome instances.`);
@@ -225,13 +261,18 @@ const main = async () => {
   let nInstances = 0;
 
   // The function that coordinates all the work.
-  // It returns a promise that fulfills when every URL is done being scraped.
+  // It returns a promise that fulfills once every URL is done being scraped.
   const run = async () => new Promise((resolve) => {
+    // The method that is called when a page
+    // is finished being scraped and the queue is empty,
+    // i.e. right before the destruction of the chrome instance.
     const onKill = () => {
       nInstances -= 1;
       // We're done when the queue is empty
       // and no chrome instance is processing something.
-      if (URLQueue.size === 0 && nInstances === 0) {
+      // This function is only called when the queue is empty so
+      // we don't explicitly check for that.
+      if (nInstances === 0) {
         resolve();
       }
     };
@@ -240,7 +281,8 @@ const main = async () => {
     // If we add an URL and we have less than
     // the maximum number of chrome instances allowed
     // for the scraping, it starts a new instance.
-    // attrs is an optional Map of attributes found on the link.
+    // attrs is an optional Map of attributes found on the link
+    // element that the URL was extracted from.
     const addURLAndScrape = async (url, attrs) => {
       const normalizedURL = normalizeURL(url);
       const shouldScrape = shouldScrapeURL(normalizedURL, attrs);
@@ -259,16 +301,19 @@ const main = async () => {
 
         // If we added some URL to scrape and we can
         // start a new instance, then we do it.
+        // It will destroy itself once it realizes the queue is empty
+        // and call onKill right before that.
         if (nInstances < nMaxInstances) {
           nInstances += 1;
-          const scraper = await createScraperProcess({
+
+          const { startScraping } = await createScraperProcess({
             addURLAndScrape,
             queueSet: URLQueue,
             seenSet: URLSeen,
             onKill,
           });
 
-          scraper.startScraping();
+          startScraping();
         }
       }
     };
--- url.js
+++ url.js
@@ -1,10 +1,17 @@
 import URL from 'url';
 
+// Generates the functions we need to interact with URLs.
+// parsedStartURL is the result of URL.parse called
+// on the URL given to the program.
 const makeURLHelpers = (parsedStartURL) => {
   // Normalizes URLs to avoid scraping the same URL twice
   // because of subtle variations in the URL string.
+  // Will remove leading and trailing whitespace,
+  // convert to lowercase and remove all trailing forward slashes.
   // Also adds the protocol for links starting in "//",
   // which the chrome driver considers invalid.
+  // Uses recursion and does a tiny bit too much of work
+  // but this is negligible and I don't like mutating variables.
   const normalizeURL = (url) => {
     if (typeof url !== 'string') {
       return '';
@@ -17,6 +24,7 @@ const makeURLHelpers = (parsedStartURL) => {
     }
 
     if (sanitizedURL.endsWith('/')) {
+      // Removes the last character if it's a forward slash.
       return normalizeURL(sanitizedURL.slice(0, -1));
     }
 
@@ -39,6 +47,8 @@ const makeURLHelpers = (parsedStartURL) => {
       return false;
     }
 
+    // It's important to use 'hostname' and not 'host'
+    // because hostname doesn't include the port if there is one.
     if (parsedURL.hostname !== parsedStartURL.hostname) {
       return false;
     }
