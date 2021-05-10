9728fac649369e406a0ce39821668e10ec65976c
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-05-10 19:29:23 +0200

    do not trigger build in production

--- src/web-ui/server.ts
+++ src/web-ui/server.ts
@@ -7,25 +7,24 @@ import webpackConfig from '../../webpack.config';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-console.log(`Running in "${process.env.NODE_ENV}" environment.`);
-
-const webpackCompiler = webpack(webpackConfig);
-
-const devMiddleware = require('webpack-dev-middleware')(webpackCompiler, {
-  publicPath: webpackConfig.output.publicPath,
-});
-
-const hotMiddleware = require('webpack-hot-middleware')(webpackCompiler);
-
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+console.log(`Running in "${process.env.NODE_ENV}" environment.`);
 
 if (isDevelopment) {
+  const webpackCompiler = webpack(webpackConfig);
+
+  const devMiddleware = require('webpack-dev-middleware')(webpackCompiler, {
+    publicPath: webpackConfig.output.publicPath,
+  });
+
+  const hotMiddleware = require('webpack-hot-middleware')(webpackCompiler);
+
   app.use(devMiddleware);
   app.use(hotMiddleware);
 }
 
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
