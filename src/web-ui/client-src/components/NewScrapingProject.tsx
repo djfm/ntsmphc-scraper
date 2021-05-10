f1c5d51522df687337f5e1be2557acd51a11deac
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-05-10 21:26:31 +0200

    start URL validation
    had to rewrite the isValidURL method, because,
    you know, webpack...
    
    Wouldn't import something usable for 'url'.

--- src/web-ui/client-src/components/NewScrapingProject.tsx
+++ src/web-ui/client-src/components/NewScrapingProject.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState, BaseSyntheticEvent } from 'react';
 
-import {
-  isValidURL,
-} from '../../../util/url';
+import { URL } from 'whatwg-url';
 
-const NewScrapingProject = () => (
-  <main>
-    <h1>Create New Scraping Project</h1>
+const isValidURL = (url: string) => {
+  try {
+    // eslint-disable-next-line no-new
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
 
-    <section>
-      <form>
-        <label>
-          Start URL
-          <p>
-            <input type="text" />
-          </p>
-        </label>
-      </form>
-    </section>
-  </main>
-);
+const NewScrapingProject = () => {
+  const [startURL, setStartURL] = useState('');
+  const [isStartURLValid, setStartURLValid] = useState(undefined);
+
+  const handleStartURLChange = ((event: BaseSyntheticEvent) => {
+    const url = event.target.value;
+    setStartURLValid(isValidURL(url));
+    setStartURL(url);
+  });
+
+  // eslint-disable-next-line react/jsx-curly-brace-presence
+  const urlErr = (<p>This URL {"doesn't"} look good.</p>);
+  const urlOK = (<p>✔ Looks good!</p>);
+  // eslint-disable-next-line no-nested-ternary
+  const urlFeedback = (isStartURLValid === undefined) ? '' : (
+    isStartURLValid ? urlOK : urlErr
+  );
+
+  const ui = (
+    <main>
+      <h1>Create New Scraping Project</h1>
+
+      <section>
+        <form>
+          <label>
+            Start URL
+            <p>
+              <input type="text" value={startURL} onChange={handleStartURLChange} />
+            </p>
+            <p>
+              {urlFeedback}
+            </p>
+          </label>
+        </form>
+      </section>
+    </main>
+  );
+
+  return ui;
+};
 
 export default NewScrapingProject;
