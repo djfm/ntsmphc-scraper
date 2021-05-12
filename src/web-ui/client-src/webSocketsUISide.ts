6d89f88b20a795775a9e0849b2788719649a78f4
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-05-12 18:37:18 +0200

    refactorization of web UI sockets stuff

--- src/web-ui/client-src/webSocketsUISide.ts
+++ src/web-ui/client-src/webSocketsUISide.ts
@@ -18,39 +18,24 @@ let socket: WebSocket;
 let nextRequestId = 0;
 const pendingRequests = new Map<number, PromiseSettler>();
 
-const connectToServer = async () => new Promise<WebSocket>((resolveConnection) => {
+const createNewSocket = async (): Promise<WebSocket> => {
   const s = new WebSocket(`ws://${host}/wss-internal`);
-  s.addEventListener('open', () => {
-    resolveConnection(s);
-  });
 
-  s.addEventListener('message', (event) => {
-    // eslint-disable-next-line no-console
-    console.log(`Currently, ${pendingRequests.size} requests are pending.`);
-    try {
-      const data = JSON.parse(event.data);
-      if (pendingRequests.has(data.id)) {
-        const { resolve, reject } = pendingRequests.get(data.id);
-        if (data.err) {
-          reject(data.err);
-        } else {
-          resolve(data.response);
-        }
-        pendingRequests.delete(data.id);
-        // eslint-disable-next-line no-console
-        console.log(`I settled one, so now, ${pendingRequests.size} are pending.`);
-      } else {
-        // eslint-disable-next-line no-console
-        console.error('Received JSON message I could not understand: ', data);
-      }
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error(err);
+  return new Promise<WebSocket>((resolve, reject) => {
+    if (s.readyState === 1) {
+      resolve(s);
+    } else if (s.readyState !== 0) {
+      reject(new Error('Could not create socket.'));
+      return;
     }
-  });
-});
 
-export const askServer = async (action: string, params: object) => {
+    s.addEventListener('open', () => resolve(s));
+  });
+};
+
+const ensureConnectionToServer = async (): Promise<WebSocket> => {
+  const oldSocket = socket;
+
   // TODO check that a new socket is really obtained when needed
   // readyState can be:
   //  - 0 => CONNECTING
@@ -59,8 +44,47 @@ export const askServer = async (action: string, params: object) => {
   //  - 3 => CLOSED
   // see https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/readyState
   if (!socket || socket.readyState >= 2) {
-    socket = await connectToServer();
+    // eslint-disable-next-line no-console
+    console.warn('[WS] creating a new socket connection to our server...');
+    socket = await createNewSocket();
   }
+
+  // we got a new socket, so we need to setup
+  // its handlers for the app to function properly
+  if (oldSocket !== socket) {
+    socket.addEventListener('message', (event) => {
+      // eslint-disable-next-line no-console
+      console.log(`Currently, ${pendingRequests.size} requests are pending.`);
+      try {
+        const data = JSON.parse(event.data);
+        if (pendingRequests.has(data.id)) {
+          const { resolve, reject } = pendingRequests.get(data.id);
+          if (data.err) {
+            reject(data.err);
+          } else {
+            resolve(data.response);
+          }
+          pendingRequests.delete(data.id);
+          // eslint-disable-next-line no-console
+          console.log(`I settled one, so now, ${pendingRequests.size} are pending.`);
+        } else {
+          // eslint-disable-next-line no-console
+          console.error('Received JSON message I could not understand: ', data);
+        }
+      } catch (err) {
+        // this would likely happen if we received something
+        // that is not proper JSON
+        // eslint-disable-next-line no-console
+        console.error(err);
+      }
+    });
+  }
+
+  return socket;
+};
+
+export const askServer = async (action: string, params: object) => {
+  const s = await ensureConnectionToServer();
 
   const request = {
     id: nextRequestId,
@@ -68,7 +92,7 @@ export const askServer = async (action: string, params: object) => {
     params,
   };
 
-  socket.send(JSON.stringify(request));
+  s.send(JSON.stringify(request));
 
   let resolver: PromiseSettler;
 
