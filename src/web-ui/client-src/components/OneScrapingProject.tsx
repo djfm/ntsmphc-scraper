9228467735374754eb977292242a0e0b026fb722
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-05-13 01:25:04 +0200

    I was passing the arguments the wrong way.

--- src/web-ui/client-src/components/OneScrapingProject.tsx
+++ src/web-ui/client-src/components/OneScrapingProject.tsx
@@ -72,7 +72,7 @@ const OneScrapingProject = () => {
   const handleStartScraping = (event: BaseSyntheticEvent) => {
     event.preventDefault();
 
-    askServer('startScraping', { project }).then(
+    askServer('startScraping', project).then(
       (res: any) => {
 
       },
